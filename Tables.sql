34fde207447da89db2e9bf13456d939d768c0a07
Arganata <arganata.on@gmail.com>
2025-03-23 08:58:41 +0700

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -5,3 +5,18 @@ CREATE TABLE employees (
     hourly_pay DECIMAL(5, 2),
     hire_date DATE
 );
+SELECT * FROM employees;
+RENAME TABLE workers TO employees;
+DROP TABLE employees;
+ALTER TABLE employees
+ADD phone_number VARCHAR(15);
+ALTER TABLE employees
+RENAME COLUMN phone_number to email;
+ALTER TABLE employees
+MODIFY COLUMN email VARCHAR(100);
+ALTER TABLE employees
+MODIFY email VARCHAR(100)
+after last_name;
+
+ALTER TABLE employees
+drop column email;
